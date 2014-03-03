63ba7b6f69d1eac628e94d9ab9e50ebf3ba2d6c7
trestletech <jeff.allen@trestletechnology.net>
2014-03-02 22:29:43 -0600

    Wire up client-side search.

--- public/js/todotxtpp.js
+++ public/js/todotxtpp.js
@@ -33,6 +33,7 @@
 
     $.ajax('/list')
     .done(function(data){
+      file = data;
       callback(null, data);
     })
     .fail(function(xhr, status){
@@ -69,25 +70,60 @@
 
   /**
    * Perform a search for the given string using the local file.
-   * @param searchVal The pattern for which we're searching -- can include special
-   *   characters like @ and + to search for projects or contexts.
+   * @param searchVal A string or array of strings representing the pattern for 
+   *   which we're searching -- can include special characters like @ and + to 
+   *   search for projects or contexts.
+   * @return If searchVals is a string, returns a single array of lineNumbers
+   *   which match the pattern. If searchVals is an array, returns an array
+   *   of arrays in which the outer index corresponds to the searchVals array,
+   *   and the inner indices are the line numbers on which a match was found.
    **/
-  var search = exports.search = function(searchVal){
-    if (searchVal.trim().length === 0){
-      // TODO flash warning
-      return;
+  var search = exports.search = function(searchVals){
+    var singleMode = false;
+
+    // Nest in an array
+    if (typeof searchVals !== "object"){
+      singleMode = true;
+      searchVals = [searchVals];
     }
 
-    // TODO search
+    searchVals = $.map(searchVals, function(searchVal){
+      var trimmed = searchVal.trim();
+      if (trimmed.length === 0){
+        return null;
+      }
+      return trimmed;
+    });
+
+    var matches = new Array(searchVals.length);
+    for (var i = 0; i < matches.length; i++){
+      matches[i] = [];
+    }
+
+    $.each(file.text.split('\n'), function(lineNum, line){
+      $.each(searchVals, function(valNum, searchVal){
+        if (searchVal && line.indexOf(searchVal) >= 0){
+          // 1-Indexed line numbers match the editor.
+          matches[valNum].push(lineNum+1);
+        }        
+      });
+    });
+
+    if (singleMode){
+      matches = matches[0];
+    }    
+    return matches;
   }
 
   /**
    * Execute a search for the given string and render the results.
    **/
   var renderSearch = exports.renderSearch = function(searchVal){
+    // Clear out the old search box if we're rendering results.
     $('#search-input').val('');
 
     var results = search(searchVal);
+    
     // TODO
   }
   
